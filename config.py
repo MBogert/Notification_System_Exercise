c5c977aa3ef3e5b2e14308a51e829101a26f58d2
MBogert <mabogert64@gmail.com>
2022-04-30 16:48:20 -0700

    Cleanup

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 
-MONGO_URI = 'mongodb+srv://dispatch_admin:Whatareyoudoinghere?1@dispatchcluster.otl05.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+MONGO_URI = '<uri>'
 HOST_NAME = 'localhost'
 DISPATCH_PORT = 8080
 
 # Twilio/SMS
-TWILIO_ACCT_SID = 'ACa75404d3c1ba1972b080925f24249dfb'
-TWILIO_AUTH_TKN = 'df419092917c547146a50a347d8d3019'
-TWILIO_PHONE_NUM = '+18647404384'
+TWILIO_ACCT_SID = '<sid>'
+TWILIO_AUTH_TKN = '<tkn>'
+TWILIO_PHONE_NUM = '<num>'
 TWILIO_MSG_DEFAULT = 'Welcome to Twilio'
-TWILIO_RECIPIENT_DEFAULT = '+13606897730'
+TWILIO_RECIPIENT_DEFAULT = '555-555-5555'
 
 # Email
-EMAIL_SUBJECT_DEFAULT = 'IMPORTANT DISPATCH'
-EMAIL_FROM_DEFAULT = 'voluts_dispatch@voltus.com(DO-NOT-REPLY)'
-EMAIL_RECIPIENT_DEFAULT = 'matthew.bogert@workday.com'
+EMAIL_SUBJECT_DEFAULT = 'IMPORTANT NOTIFICATION'
+EMAIL_FROM_DEFAULT = 'sender@email.com'
+EMAIL_RECIPIENT_DEFAULT = 'recipient@email.com'
 PORT_EMAIL = 1025
 
 # Test Gmail Acct
-USAIN_EMAIL = "usainvoltan@gmail.com"
-USAIN_PWD = 'A4xY5K5pFUxnJTiqk8DV'
+USAIN_EMAIL = "testgmail@gmail.com"
+USAIN_PWD = '<pwd>'
 
 # Logging
 LOG_HOME = 'logs/'
